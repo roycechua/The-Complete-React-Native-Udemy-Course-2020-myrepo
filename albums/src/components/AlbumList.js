ac357d65283fe1b320ab5811a366d66982ceaf89
Royce Chua <royce236@gmail.com>
2020-09-23 00:18:07 +0800

    Add card component for Album List

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import AlbumDetail from './AlbumDetail';
+import axios from 'axios';
 
 class AlbumList extends Component {
     state = { albums: [] };
 
-    componentWillMount(){
+    UNSAFE_componentWillMount(){
         // fetch("https://rallycoding.herokuapp.com/api/music_albums").then(data=>console.log(data));
         // axios.get("https://rallycoding.herokuapp.com/api/music_albums").then(response => this.setState({ albums: response.data }));
-        fetch("https://rallycoding.herokuapp.com/api/music_albums")
-            .then(response => response.json()) // convert to json first
-            .then(data => this.setState({ albums: data }));
+        // fetch("https://rallycoding.herokuapp.com/api/music_albums")
+        //     .then(response => response.json()) // convert to json first
+        //     .then(data => this.setState({ albums: data }));
+        axios.get("https://rallycoding.herokuapp.com/api/music_albums").then(response => this.setState({ albums: response.data }));
     }
 
     renderAlbums(){
