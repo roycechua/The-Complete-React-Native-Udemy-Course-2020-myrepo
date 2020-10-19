c8cb69f92851fb5724247d26712265f53d844dc1
Royce Chua <royce236@gmail.com>
2020-10-19 20:44:33 +0800

    Add firebase config and initial login screen

--- manager/App.js
+++ manager/App.js
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 import reducers from './src/reducers';
+
+import firebase from 'firebase';
+
+import LoginForm from './src/components/LoginForm';
 
 let store = createStore(reducers);
 
-const App = () => {
-  return (
-    <Provider store={store}>
-      <View>
-        <Text>Manager</Text>
-      </View>
-    </Provider>
-  );
-};
+class App extends Component {
+  componentWillMount() {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyB3feeRIsv2It3vdwWq0muxU8miPPogz28',
+      authDomain: 'manager-21bb1.firebaseapp.com',
+      databaseURL: 'https://manager-21bb1.firebaseio.com',
+      projectId: 'manager-21bb1',
+      storageBucket: 'manager-21bb1.appspot.com',
+      messagingSenderId: '791593667104',
+      appId: '1:791593667104:web:cca22abb934ff5664b8a45',
+    };
+
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    }
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <View>
+          <LoginForm/>
+        </View>
+      </Provider>
+    );
+  }
+}
 
 export default App;
 
